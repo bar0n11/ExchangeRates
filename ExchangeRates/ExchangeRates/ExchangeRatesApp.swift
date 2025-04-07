a5c1a06f2d37b66e9577b2842898d1c6099a7374
Oleg Pankiv <oleg.pankiv@gmail.com>
2025-04-07 15:16:44 -0400

    project

---
+++ ExchangeRates/ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Oleg Pankiv on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExchangeRates/ExchangeRatesTests/ExchangeRatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesTests.swift
+//  ExchangeRatesTests
+//
+//  Created by Oleg Pankiv on 07.04.2025.
+//
+
+import Testing
+@testable import ExchangeRates
+
+struct ExchangeRatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
